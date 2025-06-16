1d760d98b8a538c92979f7814b390cc79ccd48c9
Hemanthmuvvala <hemanth.m2111@gmail.com>
2025-06-16 11:52:01 +0530

    api learning

--- lib/presentation/widgets/practice.dart
+++ lib/presentation/widgets/practice.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:google_clone/models/modelclass.dart';
 import 'package:http/http.dart' as http;
@@ -8,37 +7,36 @@ class Practice extends StatefulWidget {
   const Practice({super.key});
 
   @override
-  State<Practice> createState() {
-    return _PracticeState();
-  }
+  State<Practice> createState() => _PracticeState();
 }
 
 class _PracticeState extends State<Practice> {
   bool isLoading = false;
   List<ModelClass> modelClass = [];
-  getData() async {
+
+  Future<void> getData() async {
     setState(() {
       isLoading = true;
     });
-    http.Response response =
-        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+
+    final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+
     if (response.statusCode == 200) {
-      List modelClassdata = jsonDecode(response.body);
-      modelClass = modelClassdata.map((e) => ModelClass.fromJSON(e)).toList();
+      final List decodedData = jsonDecode(response.body);
       setState(() {
-        
+        modelClass = decodedData.map((e) => ModelClass.fromJSON(e)).toList();
+        isLoading = false;
       });
+    } else {
       setState(() {
         isLoading = false;
       });
+      // Optionally handle error here
     }
-    // print(jsonDecode(data) as List);
-    //  print(data);
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getData();
   }
@@ -48,23 +46,22 @@ class _PracticeState extends State<Practice> {
     return Scaffold(
       body: isLoading
           ? const Center(
-              child: CircularProgressIndicator(
-                color: Colors.blue,
-              ),
+              child: CircularProgressIndicator(color: Colors.blue),
             )
           : ListView.builder(
-            itemCount:modelClass.length,
-            itemBuilder: (context, index) {
-              
-              return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: ListTile(
-                  title:Text(modelClass[index].title.toString()),
-                  tileColor: Colors.blue[400],
-                  subtitle:Text(modelClass[index].body),
-                ),
-              );
-            }),
+              itemCount: modelClass.length,
+              itemBuilder: (context, index) {
+                final item = modelClass[index];
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ListTile(
+                    title: Text(item.title.toString()),
+                    subtitle: Text(item.body),
+                    tileColor: Colors.blue[400],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
